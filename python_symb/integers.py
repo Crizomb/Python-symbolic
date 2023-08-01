a7461088cd45338edae18c100c73fd56bf04db94
Crizomb <62544756+Crizomb@users.noreply.github.com>
2023-08-02 00:37:52 +0200

    Delete python_symb directory

--- python_symb/integers.py
+++
@@ -1,3 +0,0 @@
-"""
-Python int is already an arbitrary precision integer, so we don't need to implement it.
-"""
--- python_symb/symbols.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-
-class Symbols:
-    __slots__ = 'name'
-
-    def __init__(self, name):
-        self.name = name
